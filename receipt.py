4611df5d9de3e5bf03c4350ea89f9af9b4b4f6ed
Erick A <>
2023-03-04 10:48:35 -0600

    Add CSV Files

---
+++ receipt.py
@@ -0,0 +1,11 @@
+def  read_dictionary ( filename, key_column_index ): 
+    """Lee el contenido de un archivo CSV en un compuesto
+    diccionario y devolver el diccionario.
+
+    Parámetros
+        filename: el nombre del archivo CSV a leer.
+        key_column_index: el índice de la columna
+            para usar como claves en el diccionario.
+    Retorno: un diccionario compuesto que contiene
+        el contenido del archivo CSV.
+    """
